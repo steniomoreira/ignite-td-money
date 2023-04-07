dbf4a80b68a094f67e5ed7fff37e90c5dc2c2ba4
steniomoreira <steniomoreiradev@gmail.com>
2023-04-07 11:51:08 -0300

    feat: transaction table structure

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionTable, TransactionTableContainer } from "./styles";
 
 export function Transaction() {
   return (
     <>    
       <Header />
       <Summary />
+
+      <TransactionTableContainer>      
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>               
+              </td>
+              <td>Venda</td>
+              <td>04/04/2023</td>
+            </tr>
+            <tr>
+              <td>Aluguel Aparmtamento</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  - R$ 1.500,00
+                </PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>10/04/2023</td>
+            </tr>            
+          </tbody>
+        </TransactionTable>
+      </TransactionTableContainer>
     </>
   )
 }
